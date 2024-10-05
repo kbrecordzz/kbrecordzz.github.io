66bc7e363ac63645f09ddd6b13bac32955b74090
kbrecordzz <87602579+kbrecordzz@users.noreply.github.com>
2024-10-05 18:29:25 +0200

    Update kbz.js

--- kbz.js
+++ kbz.js
@@ -27,34 +27,11 @@ scene.add(mesh_cloudbox2);
 
 var cloudbox_animate = 0.2; 
 
-var mobile = false;
-
-var camera_strive_x = 0;
-var camera_strive_z = 0;
-var camera_strive_y = 0;
-
-
-// intro logo
-const mesh_logo = new THREE.Mesh(new THREE.BoxGeometry(2,1.33,0.3), new THREE.MeshPhongMaterial({map: loader.load("ep3.png")}));
-var rotationrikt = 0;
-const m2 = new THREE.Mesh(new THREE.BoxGeometry(2.01,1.33,0.29), new THREE.MeshPhongMaterial({map: loader.load("blackcover.jpg")}));
-scene.add(mesh_logo);
-mesh_logo.add(m2);
-mesh_logo.rotation.y -= 1;
-mesh_logo.name = "NOTACARCLUB";
-m2.name = "NOTACARCLUB";
-
-const mesh_snowboard = new THREE.Mesh(new THREE.PlaneGeometry(1.5,0.7), new THREE.MeshPhongMaterial({map: loader.load("legendary_logo.png"), transparent: true}));
-var rotationrikt2 = 0;
-scene.add(mesh_snowboard);
-mesh_snowboard.rotation.y += 2.25;
-mesh_snowboard.name = "SNOWBOARD";
-
-
 // create actual 3d canvas (after splash screen)
 renderer = new THREE.WebGLRenderer({antialias: true});
 document.body.appendChild(renderer.domElement);		// canvas from webGLrenderer() is added to HTML document
 
+var mobile;
 if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) mobile = false;	// ej klockren!
 else mobile = true;
 
@@ -70,101 +47,6 @@ camera.position.y = 5;
 mesh_cloudbox.position.set(camera.position.x, camera.position.y+20, camera.position.z);
 mesh_cloudbox2.position.set(camera.position.x, camera.position.y-15, camera.position.z);
 
-
-var eventlock = "";		// for deciding if mouse or touch should be used (only ONE of them!)
-
-const raycaster = new THREE.Raycaster();
-const raycaster2 = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
-const mouse2 = new THREE.Vector2();
-
-function onMouseMove(event)
-{
-	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-	mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-
-	raycaster.setFromCamera(mouse, camera);
-
-	const intersects = raycaster.intersectObjects(scene.children, true);
-
-	if (intersects.length > 0)
-	{
-		const firstObject = intersects[0].object;
-		if (firstObject.name === "NOTACARCLUB") { mesh_logo.material.color = new THREE.Color(0x99FF66); document.body.style.cursor = "pointer"; }
-		else { mesh_logo.material.color = new THREE.Color(0xFFFFFF); document.body.style.cursor = "default"; }
-		
-		if (firstObject.name === "SNOWBOARD") { mesh_snowboard.material.color = new THREE.Color(0x99FF66); document.body.style.cursor = "pointer"; }
-		else { mesh_snowboard.material.color = new THREE.Color(0xFFFFFF); document.body.style.cursor = "default"; }
-	}
-}
-window.addEventListener('mousemove', onMouseMove, false);
-
-var touchstartx, touchstarty;
-
-function touch_start(e)
-{
-	if (eventlock === "") eventlock = "touch";
-
-	touchstartx = event.touches[0].clientX;
-	touchstarty = event.touches[0].clientY;
-}
-document.addEventListener("touchstart", touch_start);
-
-function touch_end(event)
-{
-	if (eventlock === "touch")
-	{
-		mouse.x = (event.changedTouches[0].clientX / window.innerWidth) * 2 - 1;
-		mouse.y = - (event.changedTouches[0].clientY / window.innerHeight) * 2 + 1;
-
-		mouse2.x = (touchstartx / window.innerWidth) * 2 - 1;
-		mouse2.y = - (touchstarty / window.innerHeight) * 2 + 1;
-
-		raycaster.setFromCamera(mouse, camera);
-		raycaster2.setFromCamera(mouse2, camera);
-
-		const intersects = raycaster.intersectObjects(scene.children, true);
-		const intersects2 = raycaster2.intersectObjects(scene.children, true);
-
-		if (intersects.length > 0 && intersects2.length > 0)
-		{
-			if (intersects[0].object.name === "NOTACARCLUB" && intersects2[0].object.name === "NOTACARCLUB") window.open("https://notacarclub.kbrecordzz.com/", "_blank");
-			else if (intersects[0].object.name === "SNOWBOARD" && intersects2[0].object.name === "SNOWBOARD") window.open("https://snowboard.kbrecordzz.com/", "_blank");
-		}
-	}
-}
-document.addEventListener("touchend", touch_end);
-
-function mouseDown(event)
-{
-	if (eventlock === "") eventlock = "mouse";
-}
-document.addEventListener("mousedown", mouseDown);
-
-function mouseUp(event)
-{
-	if (eventlock === "mouse")
-	{
-		if (event.button === 0 || event.button === 1)
-		{
-			mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-			mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-
-			raycaster.setFromCamera(mouse, camera);
-
-			const intersects = raycaster.intersectObjects(scene.children, true);
-
-			if (intersects.length > 0)
-			{
-				if (intersects[0].object.name === "NOTACARCLUB") window.open("https://notacarclub.kbrecordzz.com/", "_blank");
-				else if (intersects[0].object.name === "SNOWBOARD") window.open("https://snowboard.kbrecordzz.com/", "_blank");
-			}
-		}
-	}
-}
-document.addEventListener("mouseup", mouseUp);
-
-
 // main game loop
 function main()
 {
@@ -172,9 +54,6 @@ function main()
 	// portrait
 	if (window.innerHeight > window.innerWidth)
 	{
-		mesh_logo.visible = false;
-		mesh_snowboard.visible = false;
-
 		document.getElementById("title").style = 
 			"visibility: visible; position: absolute; top: 5%; left: 50%; transform: translateX(-50%); image-rendering: pixelated; height: 13vh;";
 
@@ -202,15 +81,6 @@ function main()
 	// landscape (mobile)
 	if (mobile === true)
 	{
-		mesh_logo.visible = true;
-		mesh_snowboard.visible = true;
-		
-		mesh_logo.scale.set(1.35,1.35,1.35);
-		mesh_logo.position.set(camera.position.x-3, camera.position.y-0.23, camera.position.z+2.66);
-
-		mesh_snowboard.scale.set(1.2,1.2,1.2);
-		mesh_snowboard.position.set(camera.position.x-3, camera.position.y-0.5, camera.position.z+1);
-
 		document.getElementById("title").style = 
 			"visibility: visible; position: absolute; top: 3%; left: 50%; transform: translateX(-50%); image-rendering: pixelated; height: 20vh;";
 
@@ -234,16 +104,7 @@ function main()
 	}
 	// landscape (desktop)
 	else
-	{
-		mesh_logo.visible = true;
-		mesh_snowboard.visible = true;
-		
-		mesh_logo.scale.set(0.8,0.8,0.8);
-		mesh_logo.position.set(camera.position.x-3, camera.position.y+0.35, camera.position.z+2.66);
-		
-		mesh_snowboard.scale.set(0.7,0.7,0.7);
-		mesh_snowboard.position.set(camera.position.x-3, camera.position.y-0.5, camera.position.z+1);
-
+	{		
 		document.getElementById("title").style = 
 			"visibility: visible; position: absolute; top: 5%; left: 50%; transform: translateX(-50%); image-rendering: pixelated; height: 13vh;";
 
@@ -265,30 +126,6 @@ function main()
 		document.getElementById("about").style = 
 		  "position: absolute; visibility: visible; bottom: 1vh; left: 1vh; font-size: 2.2vh;";
 	}
-	}
-	//
-
-	// rotate logos
-	if (rotationrikt === 0)
-	{
-		mesh_logo.rotation.y += 0.001;
-		if (mesh_logo.rotation.y > -0.5) rotationrikt = 1;
-	}
-	if (rotationrikt === 1)
-	{
-		mesh_logo.rotation.y -= 0.001
-		if (mesh_logo.rotation.y < -1) rotationrikt = 0;
-	}
-	
-	if (rotationrikt2 === 0)
-	{
-		mesh_snowboard.rotation.y += 0.003;
-		if (mesh_snowboard.rotation.y > 2.7) rotationrikt2 = 1;
-	}
-	if (rotationrikt2 === 1)
-	{
-		mesh_snowboard.rotation.y -= 0.003;
-		if (mesh_snowboard.rotation.y < 1.3) rotationrikt2 = 0;
 	}
 
 	if (mobile === true) renderer.setPixelRatio(window.devicePixelRatio*0.4);
